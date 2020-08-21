032ad39992fea57e7147c31e7b4ac858f806193e
study <>
2020-08-21 15:49:27 +0900

    db-connect

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/takuya911/golang-study
 go 1.13
 
 require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jinzhu/gorm v1.9.16
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import (
 	"log"
 
 	"github.com/labstack/echo"
+	"github.com/takuya911/golang-study/infra"
 )
 
 func main() {
+
+	// mysql connect
+	db, err := infra.NewGormDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	//
 	e := echo.New()
 	log.Fatal(e.Start(":8080"))
 }
