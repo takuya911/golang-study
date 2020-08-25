537b2aa11b8828aa6ecffa19d01349a0ba1ad587
study <>
2020-08-25 10:17:09 +0900

    server.goにmain.goの内容移設

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"log"
+
+	"github.com/labstack/echo"
+	_infra "github.com/takuya911/golang-study/infra"
+	_userHandler "github.com/takuya911/golang-study/user/handler"
+	_userRepo "github.com/takuya911/golang-study/user/repository"
+	_userUsecase "github.com/takuya911/golang-study/user/usecase"
+)
+
+func Run() {
+	// mysql connect
+	db, err := _infra.NewGormDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	e := echo.New()
+	// user services
+	userRepo := _userRepo.NewUserRepository(db)
+	userUsecase := _userUsecase.NewUserUsecase(userRepo)
+	_userHandler.NewUserHandler(e, userUsecase)
+
+	log.Fatal(e.Start(":8080"))
+}
--- main.go
+++ main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/labstack/echo"
-	_infra "github.com/takuya911/golang-study/infra"
-	_userHandler "github.com/takuya911/golang-study/user/handler"
-	_userRepo "github.com/takuya911/golang-study/user/repository"
-	_userUsecase "github.com/takuya911/golang-study/user/usecase"
-)
+import "github.com/takuya911/golang-study/api"
 
 func main() {
-	// mysql connect
-	db, err := _infra.NewGormDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		err := db.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	e := echo.New()
-	// user services
-	userRepo := _userRepo.NewUserRepository(db)
-	userUsecase := _userUsecase.NewUserUsecase(userRepo)
-	_userHandler.NewUserHandler(e, userUsecase)
-
-	log.Fatal(e.Start(":8080"))
+	api.Run()
 }
